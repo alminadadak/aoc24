45300fc14d93f19943c3cdc38b37ca059f953b3c
alminadadak <civikpadawan@gmail.com>
2024-12-03 22:47:24 +0300

    cleaned the code

--- day3/MullItOver.cpp
+++ day3/MullItOver.cpp
@@ -8,18 +8,15 @@ using namespace std;
 
 int main(){
     
-    //string text = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
     ifstream file("input.txt");
     string line;
     int result = 0;
     regex pattern(R"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))"); //restudy here
-    vector<string> matched;
     bool condition = true;
 
     if (file.is_open()) {
         while (getline(file, line)) {
             for (sregex_iterator rit(line.begin(), line.end(), pattern), end; rit != end; ++rit) {
-                matched.push_back(rit->str()); 
                 smatch match = *rit;
                 string strmatch = match.str(0);
                 cout << strmatch << endl;
